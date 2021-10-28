afa869863d32af447b691ddb4f2d2dd2348a5d12
Anthony You <you.anthony@yahoo.com>
2021-10-28 14:41:36 +0200

    feat: create singleton with all parkings

--- DouzePointCinqTest/Models/Parking.swift
+++ DouzePointCinqTest/Models/Parking.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 struct Parking: Identifiable {
+    static let allParkings = [
+        Parking(id: 1, name: "Parking Ballard", address: "16, rue des Cévennes 75015 Paris", gates: [Gate(name: "Garage", phoneNumber: 07000035551789)]),
+        Parking(id: 2, name: "Parking Leblanc", address: "71, rue Leblanc 75015 Paris", gates: [Gate(name: "Portail 1", phoneNumber: 07000035551210), Gate(name: "Portail 2", phoneNumber: 07000035551787)]),
+        Parking(id: 3, name: "Parking Procession", address: "69, rue de la Procession 75015 Paris", gates: [Gate(name: "Portail", phoneNumber: 07000035551790)])
+    ]
+
     let id: Int
     let name: String
+    let address: String
+    let gates: [Gate]
 }
