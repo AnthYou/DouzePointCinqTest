d7adbc65dd958b453e825fe38dc70ef6b5dda1ff
Anthony You <you.anthony@yahoo.com>
2021-10-28 13:01:38 +0200

    feat: deleted launch screen and add gate

---
+++ DouzePointCinqTest/Models/Gate.swift
@@ -0,0 +1,13 @@
+//
+//  Gate.swift
+//  DouzePointCinqTest
+//
+//  Created by Anthony You on 28/10/2021.
+//
+
+import Foundation
+
+struct Gate {
+    let name: String
+    let phoneNumber: Int
+}
